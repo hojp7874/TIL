5e5b7c73f343c3dd8ac21f92f65377373a01137b
Jinpyo Hong <hojp7874@gmail.com>
2020-11-14 21:44:23 +0900

    LCS Algorithm

---
+++ vue/01_sfc/lunch-lotto-with-vue-cli/src/components/CaptainYoungji.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CaptainYoungji',
+  data: function () {
+    return {
+      message: '이번 주에 회식이나 할까? 시간 알아서 빼둬.',
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
