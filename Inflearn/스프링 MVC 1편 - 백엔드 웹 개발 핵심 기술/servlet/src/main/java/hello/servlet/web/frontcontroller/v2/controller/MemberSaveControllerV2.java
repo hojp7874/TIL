8c67043988224d44c92fee3c8a1700cb46095a79
홍진표 <hojp7874@gmail.com>
2022-03-16 00:07:30 +0900

    MVC 구조로 프레임워크 만들어보기

--- Inflearn/스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
+++ Inflearn/스프링 MVC 1편 - 백엔드 웹 개발 핵심 기술/servlet/src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberSaveControllerV2.java
@@ -1,23 +1,24 @@
-package hello.servlet.web.servletmvc;
+package hello.servlet.web.frontcontroller.v2.controller;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
-public class MvcMemberSaveServlet extends HttpServlet {
+
+public class MemberSaveControllerV2 implements ControllerV2 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public MyView process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String username = req.getParameter("username");
         int age = Integer.parseInt(req.getParameter("age"));
@@ -28,8 +29,6 @@ public class MvcMemberSaveServlet extends HttpServlet {
         // Model에 데이터를 보관한다.
         req.setAttribute("member", member);
 
-        String viewPath = "/WEB-INF/views/save-result.jsp";
-        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
-        dispatcher.forward(req, resp);
+        return new MyView("/WEB-INF/views/save-result.jsp");
     }
 }
