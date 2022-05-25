f1522c7f440f48498d02d8049299307168bbe7de
홍진표 <hojp7874@gmail.com>
2022-05-25 21:07:52 +0900

    type conversion

---
+++ Inflearn/스프링 MVC 2편 - 백엔드 웹 개발 활용 기술/mvc2/typeconverter-start/src/main/java/hello/typeconverter/WebConfig.java
@@ -0,0 +1,27 @@
+package hello.typeconverter;
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.formatter.MyNumberFormatter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        // formatter보다 converter가 더 우선순위가 높음
+        // 둘 다 int <-> String 이기 때문에 중복됨
+//        registry.addConverter(new StringToIntegerConverter());
+//        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+
+        // 추가
+        registry.addFormatter(new MyNumberFormatter());
+    }
+}
